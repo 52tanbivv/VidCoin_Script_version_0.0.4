750b1981ecc2ed4c1633081bd37c8bea26fa91e8
ynef <admin@ynef.net>
2016-03-09 17:35:43 +0200

    Added files via upload

--- config.php
+++ config.php
@@ -4,16 +4,16 @@
 -------------------------------*/
 
 // Hostname is usually just localhost
-$hostname = "YnefsFaucet.db.12112809.hostedresource.com";
+$hostname = "";
 
 // Your database name
-$dbname = "YnefsFaucet";
+$dbname = "";
 
 // Your database username
-$dbusername = "YnefsFaucet";
+$dbusername = "";
 
 // Your database password
-$dbpassword = "Ko1Xk!l@kj";
+$dbpassword = "";
 
 /* Important settings
 -------------------------------*/
@@ -23,15 +23,15 @@ $siteTitle = 'VidCoin';
 
 // This is the ID you get after validating your site with Virool network at http://www.virool.com
 // Read more from the README.txt about setting up VIROOL
-$ViroolID = '60c86ff';
+$ViroolID = '';
 
 // These variables are your Facebook API keys. You get them after creating your app at https://developers.facebook.com/
 // Read more from the README.txt about setting up Facebook login
-$appID = '1495872057352301'; // The app id
-$appSecret = '63460568b35a81611ca2619bd56c4f2b'; // The secret key
+$appID = ''; // The app id
+$appSecret = ''; // The secret key
 
 // Your domain name WITHOUT http and www (example: internet.com or vidcoin.internet.com if script will be in a sub domain)
-$vcDomain = 'vidcoin.ynef.net';
+$vcDomain = '';
 
 // How much (in %) you want the referral bonus to be
 // Right now it's 20% so if you earn $0,01 the person who referred you earns $0,002
@@ -40,15 +40,15 @@ $refBonus = '20';
 
 // This is your admin panel password. It can contain characters like @£$€! etc.
 // Your admin panel login screen is yoursite.com/admin.php
-$adminPassword = 'Ko2jvKDKeOKvjdnf';
+$adminPassword = '';
 
 // Get these from: https://www.google.com/recaptcha if you don't want to receive spam emails through the Contact form.
 // More info in README.txt
-$siteKey = '6LfvTP4SAAAAADNzTDIe0FY6zH9ulkeOynrxzvce';
-$secretKey = '6LfvTP4SAAAAAEvOSMHB7l_3ocK6WfsPt633UKbc';
+$siteKey = '';
+$secretKey = '';
 
 // You SHOULD provide this. It is used on the Contact us page so you will be able to receive emails.
-$adminEmail = 'admin@ynef.net';
+$adminEmail = '';
 
 /* Optional settings
 -------------------------------*/
